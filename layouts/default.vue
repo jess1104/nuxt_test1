770f1c3e63eaeac1adafa2d05b5b12fa60a27476
jess1104 <jess1104jess1104@gmail.com>
2022-11-05 23:45:53 +0800

    [Added]從input給入參數，url拿參數呈現

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,17 @@
   <div>
     <header>
       <h1>Jess網站</h1>
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/about">about</NuxtLink>
-      <NuxtLink to="/article/list">article(list)頁</NuxtLink>
+      <NuxtLink class="nav" to="/">Home</NuxtLink> ||
+      <NuxtLink class="nav" to="/about">about</NuxtLink> ||
+      <NuxtLink class="nav" to="/article/list">article(list)頁</NuxtLink>
+      <NuxtLink class="nav" to="/item">item頁</NuxtLink>
     </header>
     <Nuxt />
   </div>
 </template>
+
+<style scoped>
+.nav {
+  margin-left: 10px;
+}
+</style>
---
+++ pages/item/_id.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>商品</h1>
+    <hr />
+    your item is
+    <input @keyup.enter="pushId" v-model="id" type="text" />
+    <button @click="pushId">確認</button>
+    <!-- 用route去拿params -->
+    <div v-if="this.$route.params.id">
+      商品id為: {{ this.$route.params.id }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: "",
+    };
+  },
+  methods: {
+    pushId() {
+      // console.log(this.$route);
+      // console.log(this.id);
+      this.$router.push({ params: { id: this.id } });
+      this.id = "";
+    },
+  },
+};
+</script>

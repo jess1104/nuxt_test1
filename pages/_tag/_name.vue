2b0f77aabf6f8bc1e10545699cb2cf33a1e9f1cf
jess1104 <jess1104jess1104@gmail.com>
2022-11-06 18:09:35 +0800

    [Added]接收多個參數

---
+++ pages/_tag/_name.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>詳細商品</h1>
+    <hr />
+    <div>標籤名:{{ this.$route.params.tag }}/{{ this.$route.params.name }}</div>
+  </div>
+</template>
---
+++ pages/_tag/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>商品一覽 :{{ this.$route.params.tag }}</h1>
+    <hr />
+    <ul>
+      <li><a :href="this.$route.params.tag + '/Zelda'">Zelda</a></li>
+      <li><a :href="this.$route.params.tag + '/Mario'">Mario</a></li>
+    </ul>
+  </div>
+</template>
